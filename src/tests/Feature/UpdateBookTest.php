7c1e033fd926bf9e2d53356004fe7dff1841ab0a
kdemir <44404086+kdemir@users.noreply.github.com>
2023-12-21 18:48:05 +0900

    unittesting:  update book test

---
+++ src/tests/Feature/UpdateBookTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Book;
+
+class UpdateBookTest extends TestCase
+{
+   
+    use RefreshDatabase;
+
+    public function testUpdateBook()
+    {
+        $data = [
+            'title' => 'Test Title',
+            'author' => 'Test Author',
+        ];
+        $book = Book::create($data);
+
+        $newAuthor = 'Edited Author';
+
+        $response = $this->put(route('books.update', $book), ['author' => $newAuthor]);
+        $response->assertRedirect(route('books.index'));
+        $book->refresh();
+        $this->assertEquals($newAuthor, $book->author);
+        
+    }
+}
