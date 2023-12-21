416bc322a144541dc52a09131a8741d4a0ccafb9
kdemir <44404086+kdemir@users.noreply.github.com>
2023-12-21 18:47:39 +0900

    unittesting: testing csv and xml export

---
+++ src/tests/Feature/ExportBooksCSVTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Book;
+
+class ExportBooksCSVTest extends TestCase
+{
+    use RefreshDatabase;
+    protected function createBooks()
+    {
+        $book1 = Book::create(['title' => 'Test Book 1', 'author' => 'Test Author 1']);
+        $book2 = Book::create(['title' => 'Test Book 2', 'author' => 'Test Author 2']);
+        $book3 = Book::create(['title' => 'Test Book 3', 'author' => 'Test Author 3']);
+    }
+
+    public function testExportBooksCSV()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportCSV'), ['export-option-csv' => 'books-csv']);
+        $this->assertTrue(preg_match('/^text\/csv/', $response->headers->get('Content-Type')) === 1);
+    }
+
+    public function testExportTitlesCSV()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportCSV'), ['export-option-csv' => 'titles-csv']);
+        $this->assertTrue(preg_match('/^text\/csv/', $response->headers->get('Content-Type')) === 1);
+    }
+
+    public function testExportAuthorsCSV()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportCSV'), ['export-option-csv' => 'authors-csv']);
+        $this->assertTrue(preg_match('/^text\/csv/', $response->headers->get('Content-Type')) === 1);
+
+    }
+}
---
+++ src/tests/Feature/ExportBooksXMLTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Book;
+
+class ExportBooksXMLTest extends TestCase
+{
+    use RefreshDatabase;
+    protected function createBooks()
+    {
+        $book1 = Book::create(['title' => 'Test Book 1', 'author' => 'Test Author 1']);
+        $book2 = Book::create(['title' => 'Test Book 2', 'author' => 'Test Author 2']);
+        $book3 = Book::create(['title' => 'Test Book 3', 'author' => 'Test Author 3']);
+    }
+
+    public function testExportBooksXML()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportXML'), ['export-option-xml' => 'books-xml']);
+        $response->assertHeader('Content-Type', 'application/xml');
+    }
+
+    public function testExportTitlesXML()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportXML'), ['export-option-xml' => 'titles-xml']);
+        $response->assertHeader('Content-Type', 'application/xml');
+    }
+
+    public function testExportAuthorsXML()
+    {
+        $this->withoutMiddleware();
+        $this->createBooks();
+
+        $response = $this->post(route('books.exportXML'), ['export-option-xml' => 'authors-xml']);
+        $response->assertHeader('Content-Type', 'application/xml');
+    }
+
+}
