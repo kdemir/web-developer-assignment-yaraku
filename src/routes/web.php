f0c078604a1ffbbdbb794e4386dc5eee30fdb619
kdemir <44404086+kdemir@users.noreply.github.com>
2023-12-21 20:42:43 +0900

    fixed routing: route to welcome page possible again, use resource for the BookController routing options

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,13 +11,8 @@
 |
 */
 
-//use App\Http\Controllers\BookController;
-
+Route::get('/', function () {return view('welcome');});
 Route::resource('books', BookController::class);
 Route::get('search','BookController@search')->name('books.search');
 Route::post('books/exportCSV', 'BookController@exportToCsv')->name('books.exportCSV');
 Route::post('books/exportXML', 'BookController@exportToXml')->name('books.exportXML');
-// Route::get('/', [BookController::class, 'index'])->name('books.index');
-// Route::post('/', [BookController::class, 'store'])->name('books.store');
-// Route::delete('/{id}', [BookController::class, 'destroy'])->name('books.destroy');
-//Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
